de9f6d74d77ac2c474d0d083168f051317073ac4
Nolan Kovacik <noltron000@gmail.com>
2019-01-28 11:33:46 -0800

    🗂 setting up database in index.js

--- index.js
+++ index.js
@@ -29,3 +29,26 @@ app.get('/', (req, res) => {
 // // mount remaining routes
 // app.use('/asset1', asset1);
 // app.use('/asset2', asset2);
+
+// process.env.PORT & MONGODB_URI lets the port and database be set by Heroku
+// if they don't exist, set them for local dev purposes
+const port = process.env.PORT || 8080;
+const url = process.env.MONGODB_URI || "mongodb://localhost/arcane-surge-v2";
+
+app.listen(port, () => {
+	console.log(`app listening on port http://localhost:${port}/`)
+})
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+	url,
+	{ useNewUrlParser: true },
+	(err, db) => {
+		console.log('app connected successfully to database');
+	}
+);
+
+mongoose.set('useCreateIndex', true) // removes deprication warning
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+
+module.exports = app
