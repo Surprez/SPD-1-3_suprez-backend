81633382aa177516b2542d5ee1ba0139db3adf4a
RaymondDashWu <raymond31670@hotmail.com>
2019-02-24 14:01:28 -0800

    [ADD] Boilerplate keywords.js

---
+++ static/js/keywords.js
@@ -0,0 +1,91 @@
+// Reference
+// https://goshakkk.name/array-form-inputs/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+class IncorporationForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      shareholders: [{ name: "" }]
+    };
+  }
+
+  handleNameChange = evt => {
+    this.setState({ name: evt.target.value });
+  };
+
+  handleShareholderNameChange = idx => evt => {
+    const newShareholders = this.state.shareholders.map((shareholder, sidx) => {
+      if (idx !== sidx) return shareholder;
+      return { ...shareholder, name: evt.target.value };
+    });
+
+    this.setState({ shareholders: newShareholders });
+  };
+
+  handleSubmit = evt => {
+    const { name, shareholders } = this.state;
+    alert(`Incorporated: ${name} with ${shareholders.length} shareholders`);
+  };
+
+  handleAddShareholder = () => {
+    this.setState({
+      shareholders: this.state.shareholders.concat([{ name: "" }])
+    });
+  };
+
+  handleRemoveShareholder = idx => () => {
+    this.setState({
+      shareholders: this.state.shareholders.filter((s, sidx) => idx !== sidx)
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Company name, e.g. Magic Everywhere LLC"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+
+        <h4>Keywords</h4>
+
+        {this.state.shareholders.map((shareholder, idx) => (
+          <div className="shareholder">
+            <input
+              type="text"
+              placeholder={`Keyword #${idx + 1}`}
+              value={shareholder.name}
+              onChange={this.handleShareholderNameChange(idx)}
+            />
+            <button
+              type="button"
+              onClick={this.handleRemoveShareholder(idx)}
+              className="small"
+            >
+              -
+            </button>
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={this.handleAddShareholder}
+          className="small"
+        >
+          Add Keyword
+        </button>
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<IncorporationForm />, rootElement);
