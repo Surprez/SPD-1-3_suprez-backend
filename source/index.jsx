84c21db347d42dc11f4b993999526b34c78165a0
Nolan Kovacik <noltron000@gmail.com>
2019-02-24 19:08:19 -0800

    🌬 quick update to index.jsx

--- source/index.jsx
+++ source/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./app.js";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<CommentBox />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<h1><code>index.jsx</code> has loaded</h1>
+		<App />
+	</div>,
+	document.getElementById('root')
+);
 
 registerServiceWorker();
