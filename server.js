0c4550343306a161e8be455bbed4a23930dfecf7
Keoni Murray <keonimurray45@gmail.com>
2019-03-07 11:13:17 -0800

    fixing cors issue

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.use(bodyParser.urlencoded({
 app.use(cors)
 // import auth
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // use static folder structure
 app.use(express.static(path.join(__dirname, "static")));
 
