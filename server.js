7bf036fef9b9272e78986b1e9d3af7cfc4ad2979
Keoni Murray <keonimurray45@gmail.com>
2019-03-08 15:51:52 -0800

    remove cors issue

--- server.js
+++ server.js
@@ -19,13 +19,6 @@ app.all('*', function (req, res, next) {
 	next();
 });
 
-const cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-	originWhitelist: [], // Allow all origins
-	requireHeader: ['origin', 'x-requested-with'],
-	removeHeaders: ['cookie', 'cookie2']
-})
-
 // .listen(port, host, function () {
 // 	console.log('Running CORS Anywhere on ' + host + ':' + port);
 // });
